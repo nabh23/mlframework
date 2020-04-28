552fb034b60569db5f877e25f9d4ac290197f485
nabh23 <aakash.nabh23@gmail.com>
2020-04-27 19:04:57 -0700

    update predictions, store models

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ import joblib
 from . import dispatcher
 
 TRAINING_DATA = os.environ.get("TRAINING_DATA")
+TEST_DATA = os.environ.get("TEST_DATA")
 FOLD = int(os.environ.get("FOLD"))
 MODEL = os.environ.get("MODEL")
 
@@ -22,7 +23,7 @@ FOLD_MAPPING = {
 
 if __name__ == "__main__":
     df = pd.read_csv(TRAINING_DATA)
-    #df_test = pd.read_csv(TEST_DATA)
+    df_test = pd.read_csv(TEST_DATA)
     train_df = df[df.kfold.isin(FOLD_MAPPING.get(FOLD))]
     valid_df = df[df.kfold==FOLD]
 
@@ -34,13 +35,13 @@ if __name__ == "__main__":
 
     valid_df = valid_df[train_df.columns]
 
-    label_encoders = []
+    label_encoders = {}
     for c in train_df.columns:
         lbl = preprocessing.LabelEncoder()
-        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
+        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist() + df_test[c].values.tolist()) 
         train_df.loc[:, c] = lbl.transform(train_df[c].values.tolist())
         valid_df.loc[:, c] = lbl.transform(valid_df[c].values.tolist())
-        label_encoders.append((c, lbl))
+        label_encoders[c] =  lbl
 
     # data is ready to train
     # clf = ensemble.RandomForestClassifier(n_estimators = 200, n_jobs = -1, verbose=2)
@@ -50,7 +51,8 @@ if __name__ == "__main__":
     print(metrics.roc_auc_score(yvalid, preds))
 
     joblib.dump(label_encoders, f"models/{MODEL}_{FOLD}_label_encoder.pkl")
-    joblib.dump(clf, f"models/{MODEL}.pkl")
+    joblib.dump(clf, f"models/{MODEL}_{FOLD}.pkl")
+    joblib.dump(train_df.columns, f"models/{MODEL}_{FOLD}_columns.pkl")
 
   
 
