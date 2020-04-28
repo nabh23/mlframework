e28864c2d4822423aad479395b01915d52765bba
nabh23 <aakash.nabh23@gmail.com>
2020-04-27 19:20:22 -0700

    update predictions

--- src/predict.py
+++ src/predict.py
@@ -13,29 +13,6 @@ TEST_DATA = os.environ.get("TEST_DATA")
 MODEL = os.environ.get("MODEL")
 
 
-"""
-if __name__ == "__main__":
-    df = pd.read_csv(TEST_DATA)
-
-    for FOLD in range(5):
-        encoders = joblib.load(os.path.join("models", f"{MODEL}_{FOLD}_label_encoder.pkl"))
-
-        for c in train_df.columns:
-            lbl = encoders[c]
-            #lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
-            df.loc[:, c] = lbl.transform(df[c].values.tolist())
-            
-
-    # data is ready to train
-    # clf = ensemble.RandomForestClassifier(n_estimators = 200, n_jobs = -1, verbose=2)
-    clf = joblib.load(os.path.join("models", f"{MODEL}_{FOLD}.pkl"))
-    cols = joblib.load(os.path.join("models", f"{MODEL}_{FOLD}_columns.pkl"))
-    
-
-    preds = clf.predict_proba(valid_df)[:, 1]
-    #print(metrics.roc_auc_score(yvalid, preds))
-"""
-
 def predict():
     df = pd.read_csv(TEST_DATA)
     test_idx = df['id'].values
@@ -43,6 +20,8 @@ def predict():
 
 
     for FOLD in range(5):
+        print(FOLD)
+        df = pd.read_csv(TEST_DATA)
         encoders = joblib.load(os.path.join("models", f"{MODEL}_{FOLD}_label_encoder.pkl"))
         cols = joblib.load(os.path.join("models", f"{MODEL}_{FOLD}_columns.pkl"))
         for c in cols:
